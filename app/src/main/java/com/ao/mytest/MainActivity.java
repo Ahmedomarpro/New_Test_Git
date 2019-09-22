02f30a57bd4ff1187d17f9dacbea1146c6815f49
Ahmed omar <ahmedomarpro@Gmail.com>
2019-09-22 10:02:21 +0200

    update code

--- app/src/main/java/com/ao/mytest/MainActivity.java
+++ app/src/main/java/com/ao/mytest/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 
 		Toast.makeText(this, "Route", Toast.LENGTH_SHORT).show();
+		Toast.makeText(this, "Route", Toast.LENGTH_SHORT).show();
 	}
 }
